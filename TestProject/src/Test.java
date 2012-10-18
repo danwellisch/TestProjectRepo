a27697e3dc2fa21bbae74c74d2b60860d814a14c
Dan Wellisch <dwellisch@unibsolutions.com>
2012-10-18 12:39:11 -0500

    New changes

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -8,6 +8,9 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("Hi Dan");
 		System.out.println("Hi Dan Again");
+		System.out.println("Hi Dan Again, now 3 times.");
+		System.out.println("Hi Dan Again, now 4 times.");
+		
 	}
 
 }
